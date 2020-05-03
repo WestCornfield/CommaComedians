2cfbf626a114956d45fbef0d5e57e8cc76ba179d
John <wesleycorwin@gmail.com>
2020-05-02 23:43:41 -0500

    deleted previous vanilla_JS config

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-  // jQuery methods go here...
-  $(".header").click(function(){
-    $(".header").removeClass("active");
-    $(this).addClass("active");
-  });
-});
